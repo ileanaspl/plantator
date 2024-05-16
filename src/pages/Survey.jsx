403e80e262b0340ac188cfd691aeb385018ac8d3
ileanaspl <ileana.saintpaul@gmail.com>
2024-05-16 16:13:41 +0200

    commit pre pull

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,45 +1,82 @@
-import { Link, useParams } from "react-router-dom";
-import { surveyData } from "../utils/data/surveydatas.js";
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { surveyData } from '../utils/data/surveydatas.js';
 
 function Survey() {
-  const { questionNumber } = useParams();
-  const questionNumberInt = parseInt(questionNumber);
-  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
-  const nextQuestionNumber = questionNumberInt + 1;
+	const { questionNumber } = useParams();
+	const questionNumberInt = parseInt(questionNumber);
+	const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
+	const nextQuestionNumber = questionNumberInt + 1;
+	const navigate = useNavigate();
 
-  function saveReply(answer) {
-    saveAnswers({ [questionNumber]: answer });
-  }
+	function saveReply(answer) {
+		saveAnswers({ [questionNumber]: answer });
+	}
 
-  return (
-    <div>
-      SURVEY page - {questionNumber}
-      <br />
-      Question : <p>{surveyData[questionNumber - 1].question} ?</p>
-      <br />
-      <Link to={`/survey/${prevQuestionNumber}`}>
-        <button onClick={saveReply}>Précédent</button>
-      </Link>
-      <br />
-      <Link to={`/survey/${nextQuestionNumber}`}>
-        <button onClick={saveReply}>Suivant</button>
-      </Link>
-      <br />
-      <br />
-      <Link to={`/results`}>Go to Results</Link>
-      <br />
-      <Link to={`/`}>Go to Home</Link>
-      <br />
-      <p>
-        Réponse A : {surveyData[questionNumber - 1].reponseA} <br />
-        Image A : {surveyData[questionNumber - 1].imageA}
-      </p>
-      <p>
-        Réponse B : {surveyData[questionNumber - 1].reponseB} <br />
-        Image B : {surveyData[questionNumber - 1].imageB}
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			SURVEY page - {questionNumber}
+			<br />
+			Question : <p>{surveyData[questionNumber - 1].question} ?</p>
+			<br />
+			<Link to={`/survey/${prevQuestionNumber}`}>
+				<button onClick={saveReply}>Précédent</button>
+			</Link>
+			<br />
+			<br />
+			<Link to={`/results`}>Go to Results</Link>
+			<br />
+			<Link to={`/`}>Go to Home</Link>
+			<br />
+			<Link to={`/survey/${nextQuestionNumber}`}>
+				<button onClick={saveReply}>
+					<p>
+						Réponse A : {surveyData[questionNumber - 1].reponseA} <br />
+						Image A : {surveyData[questionNumber - 1].imageA}
+					</p>
+				</button>
+			</Link>
+			<Link to={`/survey/${nextQuestionNumber}`}>
+				<button onClick={saveReply}>
+					<p>
+						Réponse B : {surveyData[questionNumber - 1].reponseB} <br />
+						Image B : {surveyData[questionNumber - 1].imageB}
+					</p>
+				</button>
+			</Link>
+			<Link to={`/survey/${nextQuestionNumber}`}>
+				{surveyData[questionNumber - 1].reponseC && (
+					<button onClick={saveReply}>
+						<p>
+							Réponse C : {surveyData[questionNumber - 1].reponseC} <br />
+							Image C : {surveyData[questionNumber - 1].imageC}
+						</p>
+					</button>
+				)}
+			</Link>
+			<Link to={`/survey/${nextQuestionNumber}`}>
+				{surveyData[questionNumber - 1].reponseD && (
+					<button onClick={saveReply}>
+						<p>
+							Réponse D : {surveyData[questionNumber - 1].reponseD} <br />
+							Image D : {surveyData[questionNumber - 1].imageD}
+						</p>
+					</button>
+				)}
+			</Link>
+			<Link to={`/survey/${nextQuestionNumber}`}>
+				{surveyData[questionNumber - 1].reponseE && (
+					<button onClick={saveReply}>
+						<p>
+							Réponse E : {surveyData[questionNumber - 1].reponseE} <br />
+							Image E : {surveyData[questionNumber - 1].imageE}
+						</p>
+					</button>
+				)}
+			</Link>
+		</div>
+	);
 }
 
 export default Survey;
+
+// {questionNumber === 6 ? to={`/survey/${nextQuestionNumber}` : to={`/results`}}
--- src/utils/data/plants.js
+++ src/utils/data/plants.js
@@ -1,37 +1,148 @@
-let plantes = [
-    {
-        anis: {
-            id: 1,
-            nom: [
-                {
-                    vernaculaire: "Anis vert"
-                },
-                {
-                    scientifique: "Pimpinella anisum"
-                }
-            ],
-            img: "../../assets/anis_vert.jpg",
-            famille: "Apiacées",
-            feuille: [
-                {
-                    folioles: "composées",
-                    decoupe: "dentelées",
-                    disposition: "alternées",
-                }
-            ],
-            fleur: [
-                {
-                    couleur: "blanche",
-                    disposition: "ombelles"
-                }
-            ],
-            fruit: [
-                {
-                    couleur: "gris verdâtre",
-                    forme: "oblongue",
-                    odeur: "forte"
-                }
-            ],
-        }
-    }
-]
+const plantes = [
+	{
+		anis: {
+			id: 1,
+			nom: [
+				{
+					vernaculaire: 'Anis vert',
+				},
+				{
+					scientifique: 'Pimpinella anisum',
+				},
+			],
+			img: '../../assets/plantes/anis_vert.jpg',
+			famille: 'Apiacées',
+			feuille: [
+				{
+					folioles: 'composées',
+					decoupe: 'dentelées',
+					disposition: 'alternées',
+				},
+			],
+			fleur: [
+				{
+					couleur: 'blanches',
+					disposition: 'ombelles',
+				},
+			],
+			geolocX: 1,
+			geolocY: 2,
+		},
+		millepertuis: {
+			id: 2,
+			nom: [
+				{
+					vernaculaire: 'Millepertuis perforé',
+				},
+				{
+					scientifique: 'Hypericum perforatum',
+				},
+			],
+			img: '../../assets/plantes/millepertuis-perfore.jpg',
+			famille: 'Apiacées',
+			feuille: [
+				{
+					folioles: 'simples',
+					decoupe: 'lisses',
+					disposition: 'opposées',
+				},
+			],
+			fleur: [
+				{
+					couleur: 'jaunes',
+					disposition: 'panicules',
+					petale: '5',
+				},
+			],
+			geolocX: 1,
+			geolocY: 2,
+		},
+		mauve: {
+			id: 3,
+			nom: [
+				{
+					vernaculaire: 'Mauve sylvestre',
+				},
+				{
+					scientifique: 'Malva sylvestris',
+				},
+			],
+			img: '../../assets/plantes/mauve-sylvestre.jpg',
+			famille: 'Malvaceae',
+			feuille: [
+				{
+					folioles: 'simples',
+					decoupe: 'dentelées',
+					disposition: 'verticillées',
+				},
+			],
+			fleur: [
+				{
+					couleur: 'violette',
+					disposition: 'fascicules',
+					petale: '5',
+				},
+			],
+			geolocX: 1,
+			geolocY: 2,
+		},
+		chicoree: {
+			id: 4,
+			nom: [
+				{
+					vernaculaire: 'Chicorée sauvage',
+				},
+				{
+					scientifique: 'Cichorium intybus',
+				},
+			],
+			img: '../../assets/plantes/chicoree-sauvage.jpg',
+			famille: 'Asteraceae',
+			feuille: [
+				{
+					folioles: 'simples',
+					decoupe: 'dentelées',
+					disposition: 'alternées',
+				},
+			],
+			fleur: [
+				{
+					couleur: 'bleues',
+					disposition: 'panicules',
+					petale: '> 15',
+				},
+			],
+			geolocX: 1,
+			geolocY: 2,
+		},
+		pavot: {
+			id: 8,
+			nom: [
+				{
+					vernaculaire: 'Pavot de Californie',
+				},
+				{
+					scientifique: 'Eschscholzia californica',
+				},
+			],
+			img: '../../assets/plantes/pavot-de-californie.jpg',
+			famille: 'Papaveraceae',
+			feuille: [
+				{
+					folioles: 'composées',
+					decoupe: 'lisses',
+					disposition: 'alternées',
+				},
+			],
+			fleur: [
+				{
+					couleur: 'oranges',
+					disposition: 'isolées',
+					petale: '4',
+				},
+			],
+			geolocX: 1,
+			geolocY: 2,
+		},
+	},
+];
