8fa722e9c5ac4b1479733e3e4455dcc33adcc27d
Modrum <tristanlaroye@hotmail.com>
2024-05-16 20:26:44 +0200

    building structure for map design

---
+++ src/utils/data/plantsLoc.js
@@ -0,0 +1,268 @@
+import anis from "../../assets/plantes/anis-vert.jpg";
+
+export const plantsLoc = [
+  {
+    id: 1,
+    nom: {
+      vernaculaire: "Anis vert",
+      scientifique: "Pimpinella anisum",
+    },
+    img: anis,
+    famille: "Apiacées",
+    feuille: {
+      folioles: "composées",
+      decoupe: "dentées",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "blanche",
+      disposition: "ombelles",
+    },
+
+    fruit: {
+      couleur: "gris verdâtre",
+      forme: "oblongue",
+      odeur: "forte",
+    },
+    partieUtil: "Semences",
+    proprietes: "antispasmodique, carminatif, galactogène",
+    indicationsTrad: "En interne : ballonements, spasmes digestifs, allaitement",
+    geolocX: 41.9000122264,
+    geolocY: 12.4478083889,
+  },
+  {
+    id: 2,
+    nom: {
+      vernaculaire: "Millepertuis perforé",
+      scientifique: "Hypericum perforatum",
+    },
+    img: "../../assets/plantes/millepertuis-perfore.jpg",
+    famille: "Apiacées",
+    feuille: {
+      folioles: "simples",
+      decoupe: "lisses",
+      disposition: "opposées",
+    },
+
+    fleur: {
+      couleur: "jaunes",
+      disposition: "panicules",
+      petale: "5",
+    },
+    partieUtil: "Sommités fleuries",
+    proprietes: "Anxiolytique, antidepressive",
+    indicationsTrad:
+      "En interne : état dépressif léger à modéré, 'coup de blues'. ATTENTION ! Ne pas utiliser en cas de prise de médicaments quelconques ou en remplacement de ceux-ci.",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 3,
+    nom: {
+      vernaculaire: "Mauve sylvestre",
+      scientifique: "Malva sylvestris",
+    },
+    img: "../../assets/plantes/mauve-sylvestre.jpg",
+    famille: "Malvaceae",
+    feuille: {
+      folioles: "simples",
+      decoupe: "dentées",
+      disposition: "verticillées",
+    },
+
+    fleur: {
+      couleur: "violette",
+      disposition: "fascicules",
+      petale: "5",
+    },
+    partieUtil: "Fleurs",
+    proprietes: "Émollientes, anti-inflammatoires, adoucissantes, laxatif léger",
+    indicationsTrad:
+      "En interne : inflammations digestives (estomac, intestins), gorges irritées, toux sèche, constipatio chronique. En externe : en cataplasme pour apaiser les petites irritations de la peau",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 4,
+    nom: {
+      vernaculaire: "Chicorée sauvage",
+      scientifique: "Cichorium intybus",
+    },
+    img: "../../assets/plantes/chicoree-sauvage.jpg",
+    famille: "Asteraceae",
+    feuille: {
+      folioles: "simples",
+      decoupe: "dentées",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "bleues",
+      disposition: "panicules",
+      petale: "> 15",
+    },
+    partieUtil: "Racines",
+    proprietes: "Choléretique et cholagogue, apéritive, prébiotique",
+    indicationsTrad:
+      'En interne : dyspépsies, troubles de la digestion accompagnés de nausées, ballonements, etc. Pour stimuler l"appétit',
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 5,
+    nom: {
+      vernaculaire: "Achillée millefeuille",
+      scientifique: "Achillea millefolium",
+    },
+    img: "../../assets/plantes/achille-millefolium.jpg",
+    famille: "Asteraceae",
+    feuille: {
+      folioles: "simple",
+      decoupe: "pennées",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "blanches",
+      disposition: "corymbes",
+    },
+
+    partieUtil: "Sommités fleuries",
+    proprietes: "Hémostatique, emménagogue, antispasmodique et décongestionante",
+    indicationsTrad:
+      "En infusion : soulagement des spasmes digestifs et menstruels, réduire les saignements trop abondants lors des règles. En externe : stopper les saignements (plaies, coupures)",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 6,
+    nom: {
+      vernaculaire: "Grande aunée",
+      scientifique: "Inula helenium",
+    },
+    img: "../../assets/plantes/grande-aunee.jpg",
+    famille: "Asteraceae",
+    feuille: {
+      folioles: "simples",
+      decoupe: "dentées",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "jaunes",
+      disposition: "capitules",
+    },
+
+    partieUtil: "Racine",
+    proprietes:
+      "Bronchodilatatrice, expectorante, prébiotque, choléretique, cholagogue, diurétique",
+    indicationsTrad:
+      "En interne : encombrement des poumons, infections des bronches ou des poumons, ralentissement des fonctions d'éliminations de l'organisme",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 7,
+    nom: {
+      vernaculaire: "Epilobe à petite fleur",
+      scientifique: "Epilobium parviflorum",
+    },
+    img: "../../assets/plantes/epilobe-a-petites-fleurs.jpg",
+    famille: "Oenotheraceae",
+    feuille: {
+      folioles: "simple",
+      decoupe: "dentées",
+      disposition: "opposées",
+    },
+
+    fleur: {
+      couleur: "jaunes",
+      disposition: "capitules",
+      petale: "4",
+    },
+
+    partieUtil: "Tiges et feuilles",
+    proprietes: "Émollient et astringeant",
+    indicationsTrad: "ballonement, spasmes digestifs, allaitement",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 8,
+    nom: {
+      vernaculaire: "Pavot de Californie",
+      scientifique: "Eschscholzia californica",
+    },
+
+    img: "../../assets/plantes/pavot-de-californie.jpg",
+    famille: "Papaveraceae",
+    feuille: {
+      folioles: "composées",
+      decoupe: "lisses",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "oranges",
+      disposition: "isolées",
+      petale: "4",
+    },
+    partieUtil: "Parties aériennes fleuries",
+    proprietes: "Sédatives et hypnotiques, anxiolytiques",
+    indicationsTrad: "En interne : pour faciliter l'endormissement en cas d'insomnies et de stress",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 9,
+    nom: {
+      vernaculaire: "Digitale pourpre",
+      scientifique: "Digitalis purpurea",
+    },
+    img: "../../assets/plantes/digitale-pourpre.jpg",
+    famille: "Scrophulariaceae",
+    feuille: {
+      folioles: "simple",
+      decoupe: "dentées",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "rose",
+      disposition: "grappes",
+    },
+    proprietes:
+      "Plante extrêmement toxique ! Certains de ses principes actifs comme la dioxine sont utilisés à des dosages infimes dans des médicaments comme cardiotonique",
+    geolocX: 1,
+    geolocY: 2,
+  },
+  {
+    id: 10,
+    nom: {
+      vernaculaire: "Bourrache officinale",
+      scientifique: "Borago officinalis",
+    },
+    img: "../../assets/plantes/bourrache-officinale.jpg",
+    famille: "Boraginaceae",
+    feuille: {
+      folioles: "simple",
+      decoupe: "poilues",
+      disposition: "alternées",
+    },
+
+    fleur: {
+      couleur: "bleues",
+      disposition: "panicules",
+      petale: "5",
+    },
+    partieUtil: "Graines (huile)",
+    proprietes:
+      "Anti-inflammatoire, émollient et humidifiant en voie cutanée de la peau et des muqueuses",
+    indicationsTrad:
+      "En interne : pour l'apport d'acide gamma-linolénique (précurseur des prostaglandines anti-inflammatoires. Notamment utile dans certains cas pour soulager le syndrome prémenstruel ou à la peri-ménopause. En externe : pour les peaux ou muqueuses sèches.",
+
+    geolocX: 1,
+    geolocY: 2,
+  },
+];
