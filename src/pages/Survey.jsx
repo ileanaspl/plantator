21bb560868abe53f5bf28dffc53f8839fab4a967
Modrum <tristanlaroye@hotmail.com>
2024-05-16 14:45:30 +0200

    Questionnaire

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import { surveyData } from "../utils/data/surveydatas.js";
 
 function Survey() {
   const { questionNumber } = useParams();
@@ -6,19 +7,37 @@ function Survey() {
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
   const nextQuestionNumber = questionNumberInt + 1;
 
+  function saveReply(answer) {
+    saveAnswers({ [questionNumber]: answer });
+  }
+
   return (
     <div>
-      SURVEY {questionNumber}
+      SURVEY page - {questionNumber}
       <br />
+      Question : <p>{surveyData[questionNumber - 1].question} ?</p>
       <br />
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+      <Link to={`/survey/${prevQuestionNumber}`}>
+        <button onClick={saveReply}>Précédent</button>
+      </Link>
       <br />
-      <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+      <Link to={`/survey/${nextQuestionNumber}`}>
+        <button onClick={saveReply}>Suivant</button>
+      </Link>
       <br />
       <br />
       <Link to={`/results`}>Go to Results</Link>
       <br />
       <Link to={`/`}>Go to Home</Link>
+      <br />
+      <p>
+        Réponse A : {surveyData[questionNumber - 1].reponseA} <br />
+        Image A : {surveyData[questionNumber - 1].imageA}
+      </p>
+      <p>
+        Réponse B : {surveyData[questionNumber - 1].reponseB} <br />
+        Image B : {surveyData[questionNumber - 1].imageB}
+      </p>
     </div>
   );
 }
