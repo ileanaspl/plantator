349fda57f04700a6e1acb4f4886b782093c799e9
BrebG <brebelgael@protonmail.com>
2024-05-16 15:28:01 +0200

    add plants in array and img

--- src/utils/data/plants.js
+++ src/utils/data/plants.js
@@ -1,17 +1,13 @@
-let plantes = [
+const plantes = [
     {
         anis: {
             id: 1,
             nom: [
-                {
-                    vernaculaire: "Anis vert"
-                },
-                {
-                    scientifique: "Pimpinella anisum"
-                }
+                { vernaculaire: "Anis vert" },
+                { scientifique: "Pimpinella anisum" }
             ],
-            img: "../../assets/anis_vert.jpg",
-            famille: "Apiacées",
+            img: "../../assets/plantes/anis-vert.jpg",
+            famille: "Apiaceae",
             feuille: [
                 {
                     folioles: "composées",
@@ -21,17 +17,134 @@ let plantes = [
             ],
             fleur: [
                 {
-                    couleur: "blanche",
+                    couleur: "blanches",
                     disposition: "ombelles"
                 }
             ],
-            fruit: [
+            geolocX: 1,
+            geolocY: 2,
+        },
+        achillee: {
+            id: 5,
+            nom: [
+                { vernaculaire: "Achillée millefeuille" },
+                { scientifique: "Achillea millefolium" }
+            ],
+            img: "../../assets/plantes/achille-millefolium.jpg",
+            famille: "Asteraceae",
+            feuille: [
                 {
-                    couleur: "gris verdâtre",
-                    forme: "oblongue",
-                    odeur: "forte"
+                    folioles: "simple",
+                    decoupe: "pennées",
+                    disposition: "alternées",
                 }
             ],
-        }
+            fleur: [
+                {
+                    couleur: "blanches",
+                    disposition: "corymbes"
+                }
+            ],
+            geolocX: 1,
+            geolocY: 2,
+        },
+        aunee: {
+            id: 6,
+            nom: [
+                { vernaculaire: "Grande aunée" },
+                { scientifique: "Inula helenium" }
+            ],
+            img: "../../assets/plantes/grande-aunee.jpg",
+            famille: "Asteraceae",
+            feuille: [
+                {
+                    folioles: "simples",
+                    decoupe: "dentées",
+                    disposition: "alternées",
+                }
+            ],
+            fleur: [
+                {
+                    couleur: "jaunes",
+                    disposition: "capitules",
+                }
+            ],
+            geolocX: 1,
+            geolocY: 2,
+        },
+        epilobe: {
+            id: 7,
+            nom: [
+                { vernaculaire: "Epilobe à petite fleur" },
+                { scientifique: "Epilobium parviflorum" }
+            ],
+            img: "../../assets/plantes/epilobe-a-petites-fleurs.jpg",
+            famille: "Oenotheraceae",
+            feuille: [
+                {
+                    folioles: "simple",
+                    decoupe: "dentées",
+                    disposition: "opposées",
+                }
+            ],
+            fleur: [
+                {
+                    couleur: "jaunes",
+                    disposition: "capitules",
+                    petale: "4"
+                }
+            ],
+            geolocX: 1,
+            geolocY: 2,
+        },
+        digitale: {
+            id: 9,
+            nom: [
+                { vernaculaire: "Digitale pourpre" },
+                { scientifique: "Digitalis purpurea" }
+            ],
+            img: "../../assets/plantes/digitale-pourpre.jpg",
+            famille: "Scrophulariaceae",
+            feuille: [
+                {
+                    folioles: "simple",
+                    decoupe: "dentées",
+                    disposition: "alternées",
+                }
+            ],
+            fleur: [
+                {
+                    couleur: "pourpres",
+                    disposition: "grappes",
+                }
+            ],
+            geolocX: 1,
+            geolocY: 2,
+        },
+        bourrache: {
+            id: 10,
+            nom: [
+                { vernaculaire: "Bourrache officinale" },
+                { scientifique: "Borago officinalis" }
+            ],
+            img: "../../assets/plantes/bourrache-officinale.jpg",
+            famille: "Boraginaceae",
+            feuille: [
+                {
+                    folioles: "simple",
+                    decoupe: "poilues",
+                    disposition: "alternées",
+                }
+            ],
+            fleur: [
+                {
+                    couleur: "bleues",
+                    disposition: "panicules",
+                    petale: "5"
+                }
+            ],
+            geolocX: 1,
+            geolocY: 2,
+        },
     }
 ]
