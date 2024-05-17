d4aef9b64fbc48dc12d080f63328d4f3071507ea
BrebG <brebelgael@protonmail.com>
2024-05-17 15:03:40 +0200

    commit before pull, add css results page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -8,62 +8,63 @@ import { SurveyContext } from "../utils/context/SurveyContext";
 import "./Results.scss";
 
 function Results() {
-  const [selectedPlants, setSelectedPlants] = useState("");
-  const [winningPlant, setWinningPlant] = useState("");
-  const [datas, setDatas] = useState([]);
-  const { answers } = useContext(SurveyContext);
-  const makeResults = () =>
-    plantes.filter(
-      (obj) =>
-        obj.feuille.folioles === answers[1] &&
-        obj.feuille.decoupe === answers[2] &&
-        obj.feuille.disposition === answers[3] &&
-        obj.fleur.couleur === answers[4] &&
-        obj.fleur.disposition === answers[5] &&
-        obj.fleur.petale === answers[6],
-    );
+	const [selectedPlants, setSelectedPlants] = useState("");
+	const [winningPlant, setWinningPlant] = useState("");
+	const [datas, setDatas] = useState([]);
+	const { answers } = useContext(SurveyContext);
+	const makeResults = () =>
+		plantes.filter(
+			(obj) =>
+				obj.feuille.folioles === answers[1] &&
+				obj.feuille.decoupe === answers[2] &&
+				obj.feuille.disposition === answers[3] &&
+				obj.fleur.couleur === answers[4] &&
+				obj.fleur.disposition === answers[5] &&
+				obj.fleur.petale === answers[6],
+		);
 
-  useEffect(() => {
-    const resultsSurvey = makeResults();
+	useEffect(() => {
+		const resultsSurvey = makeResults();
 
-    if (resultsSurvey.length > 0) {
-      setSelectedPlants(resultsSurvey[0].nom?.vernaculaire);
-      setWinningPlant(resultsSurvey[0].nom?.vernaculaire);
-    } else {
-      setSelectedPlants("");
-      setWinningPlant("nowinner");
-    }
-  }, []);
+		if (resultsSurvey.length > 0) {
+			setSelectedPlants(resultsSurvey[0].nom?.vernaculaire);
+			setWinningPlant(resultsSurvey[0].nom?.vernaculaire);
+		} else {
+			setSelectedPlants("");
+			setWinningPlant("nowinner");
+		}
+	}, []);
 
-  const changePlants = () => {
-    return plantes.filter((obj) =>
-      selectedPlants !== "" ? obj.nom.vernaculaire === selectedPlants : obj,
-    );
-  };
+	const changePlants = () => {
+		return plantes.filter((obj) =>
+			selectedPlants !== "" ? obj.nom.vernaculaire === selectedPlants : obj,
+		);
+	};
 
-  useEffect(() => {
-    setDatas(changePlants());
-  }, [selectedPlants]);
+	useEffect(() => {
+		setDatas(changePlants());
+	}, [selectedPlants]);
 
-  return (
-    <div>
-      RESULTS
-      <br />
-      {datas.length >= 1 && answers[1] && winningPlant !== "" && (
-        <h2>
-          {winningPlant === "nowinner"
-            ? "Aucune plante ne correspond à vos critères"
-            : `La plante qui correspond à vos critères est la ${winningPlant} `}
-        </h2>
-      )}
-      {selectedPlants !== "" && <h3>Plante recherchée : {selectedPlants}</h3>}
-      <br />
-      <Link to={`/`}>Retour à l'accueil</Link>
-      <br />
-      <DropdownSelectorPlants setSelectedPlants={setSelectedPlants} />
-      {datas && <GreenGlobe datas={datas} />}
-    </div>
-  );
+	return (
+		<div>
+			<div className="topSection">
+				<Link to={`/`} className="navLink">Retour à l'accueil</Link>
+				<br />
+				<h1>RÉSULTATS</h1>
+				<br />
+				{datas.length >= 1 && answers[1] && winningPlant !== "" && (
+					<h2>
+						{winningPlant === "nowinner"
+							? "Aucune plante ne correspond à vos critères"
+							: `La plante qui correspond à vos critères est la ${winningPlant} `}
+					</h2>
+				)}
+				{selectedPlants !== "" && <h3>Plante recherchée : {selectedPlants}</h3>}
+			</div>
+			<DropdownSelectorPlants setSelectedPlants={setSelectedPlants} />
+			{datas && <GreenGlobe datas={datas} />}
+		</div>
+	);
 }
 
 export default Results;
