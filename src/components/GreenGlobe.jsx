503e1b3224fc2f3cd0d49f25e92388740a162992
Modrum <tristanlaroye@hotmail.com>
2024-05-17 10:48:44 +0200

    fining planet

--- src/components/GreenGlobe.jsx
+++ src/components/GreenGlobe.jsx
@@ -32,7 +32,7 @@ function GreenGlobe({ datas }) {
     const globe = globeEl.current;
     // Auto-rotate
     globe.controls().autoRotate = true;
-    globe.controls().autoRotateSpeed = 0.1;
+    globe.controls().autoRotateSpeed = -0.1;
 
     // Center the view on the specified coordinates
     if (places.length > 0) {
@@ -40,9 +40,8 @@ function GreenGlobe({ datas }) {
       const centerCoords = {
         lat: places[1].coordonnees.lat,
         lng: places[1].coordonnees.lng,
-        altitude: 2,
+        altitude: 1.8,
       };
-      console.log(centerCoords);
       globe.pointOfView(centerCoords, 0);
     }
   }, [places]);
@@ -51,8 +50,8 @@ function GreenGlobe({ datas }) {
     const globe = globeEl.current;
     // Add clouds sphere
     const CLOUDS_IMG_URL = cloud; // from https://github.com/turban/webgl-earth
-    const CLOUDS_ALT = 0.001;
-    const CLOUDS_ROTATION_SPEED = -0.009; // deg/frame
+    const CLOUDS_ALT = 0.005;
+    const CLOUDS_ROTATION_SPEED = 0.009; // deg/frame
 
     // Charger la texture des nuages
     new THREE.TextureLoader().load(
@@ -110,6 +109,8 @@ function GreenGlobe({ datas }) {
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        atmosphereColor="lightskyblue"
+        atmosphereAltitude={0.4}
         labelsData={places}
         labelLat={(d) => d.coordonnees.lat}
         labelLng={(d) => d.coordonnees.lng}
@@ -118,7 +119,7 @@ function GreenGlobe({ datas }) {
         labelDotRadius={(d) => 1}
         labelColor={(d) => d.labelcolor}
         labelResolution={2}
-        labelAltitude={0.01}
+        labelAltitude={(d) => d.id * 0.01}
         onLabelHover={(label) => {
           handleLabelHover(label);
         }}
