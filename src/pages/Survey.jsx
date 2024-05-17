eb24a2ad52091e343401c3b1a4fc0a3f2f8e3b15
ileanaspl <ileana.saintpaul@gmail.com>
2024-05-17 11:11:00 +0200

    filtrer plants in result

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,95 +1,91 @@
-import { Link, useParams } from "react-router-dom";
-import { surveyData } from "../utils/data/surveydatas.js";
-import { useContext } from "react";
-import { SurveyContext } from "../utils/context/SurveyContext.jsx";
+import { Link, useParams } from 'react-router-dom';
+import { surveyData } from '../utils/data/surveydatas.js';
+import { useContext } from 'react';
+import { SurveyContext } from '../utils/context/SurveyContext.jsx';
+import { div } from 'three/examples/jsm/nodes/Nodes.js';
 
 function Survey() {
-  const { questionNumber } = useParams();
-  const questionNumberInt = parseInt(questionNumber);
-  const { answers, saveAnswers } = useContext(SurveyContext);
-  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
-  const nextQuestionNumber = questionNumberInt + 1;
-  const nextPath = questionNumber >= 6 ? `/results` : `/survey/${nextQuestionNumber}`;
+	const { questionNumber } = useParams();
+	const questionNumberInt = parseInt(questionNumber);
+	const { answers, saveAnswers } = useContext(SurveyContext);
+	const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
+	const nextQuestionNumber = questionNumberInt + 1;
+	const nextPath = questionNumber >= 6 ? `/results` : `/survey/${nextQuestionNumber}`;
 
-  function saveReply(answer) {
-    saveAnswers({ [questionNumber]: answer });
-  }
+	function saveReply(answer) {
+		saveAnswers({ [questionNumber]: answer });
+	}
 
-  return (
-    <div>
-      {console.log(answers)}
-      SURVEY page - {questionNumber}
-      <br />
-      Question : <p>{surveyData[questionNumber - 1].question}</p>
-      <br />
-      <br />
-      <br />
-      <Link to={`/results`}>Go to Results</Link>
-      <br />
-      <Link to={`/`}>Go to Home</Link>
-      <br />
-      <div className="grandcontainer-a" style={{ border: "solid 1px red" }}>
-        <Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseA)}>
-          Réponse A :<p>{surveyData[questionNumber - 1].reponseA}</p>
-          <br />
-          <img src={surveyData[questionNumber - 1].imageA} />
-        </Link>
-      </div>
-      <Link to={nextPath}>
-        <button onClick={saveReply}>
-          <p>
-            Réponse B : {surveyData[questionNumber - 1].reponseB} <br />
-            Image B : {surveyData[questionNumber - 1].imageB}
-          </p>
-        </button>
-      </Link>
-      {surveyData[questionNumber - 1].reponseC && (
-        <Link to={nextPath}>
-          <button onClick={saveReply}>
-            <p>
-              Réponse C : {surveyData[questionNumber - 1].reponseC} <br />
-              Image C : {surveyData[questionNumber - 1].imageC}
-            </p>
-          </button>
-        </Link>
-      )}
-      {surveyData[questionNumber - 1].reponseD && (
-        <Link to={nextPath}>
-          <button onClick={saveReply}>
-            <p>
-              Réponse D : {surveyData[questionNumber - 1].reponseD} <br />
-              Image D : {surveyData[questionNumber - 1].imageD}
-            </p>
-          </button>
-        </Link>
-      )}
-      {surveyData[questionNumber - 1].reponseE && (
-        <Link to={nextPath}>
-          <button onClick={saveReply}>
-            <p>
-              Réponse E : {surveyData[questionNumber - 1].reponseE} <br />
-              Image E : {surveyData[questionNumber - 1].imageE}
-            </p>
-          </button>
-        </Link>
-      )}
-      {surveyData[questionNumber - 1].reponseF && (
-        <Link to={nextPath}>
-          <button onClick={saveReply}>
-            <p>
-              Réponse F : {surveyData[questionNumber - 1].reponseF} <br />
-              Image F : {surveyData[questionNumber - 1].imageF}
-            </p>
-          </button>
-        </Link>
-      )}
-      <br />
-      <br />
-      <Link to={`/survey/${prevQuestionNumber}`}>
-        <button onClick={saveReply}>Précédent</button>
-      </Link>
-    </div>
-  );
+	return (
+		<div>
+			{console.log(answers)}
+			SURVEY page - {questionNumber}
+			<br />
+			Question : <p>{surveyData[questionNumber - 1].question}</p>
+			<br />
+			<br />
+			<br />
+			<Link to={`/results`}>Go to Results</Link>
+			<br />
+			<Link to={`/`}>Go to Home</Link>
+			<br />
+			<div id="grandcontainer">
+				<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseA)}>
+					<p>{surveyData[questionNumber - 1].reponseA}</p>
+					<br />
+					<img src={surveyData[questionNumber - 1].imageA} />
+				</Link>
+				<div className="grandcontainer-a">
+					<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseB)}>
+						<p>{surveyData[questionNumber - 1].reponseB}</p>
+						<br />
+						<img src={surveyData[questionNumber - 1].imageB} />
+					</Link>
+				</div>
+				{surveyData[questionNumber - 1].reponseC && (
+					<div className="grandcontainer-a">
+						<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseC)}>
+							<p>{surveyData[questionNumber - 1].reponseC}</p>
+							<br />
+							<img src={surveyData[questionNumber - 1].imageC} />
+						</Link>
+					</div>
+				)}
+				{surveyData[questionNumber - 1].reponseD && (
+					<div className="grandcontainer-a">
+						<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseD)}>
+							<p>{surveyData[questionNumber - 1].reponseD}</p>
+							<br />
+							<img src={surveyData[questionNumber - 1].imageD} />
+						</Link>
+					</div>
+				)}
+				{surveyData[questionNumber - 1].reponseE && (
+					<div className="grandcontainer-a">
+						<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseE)}>
+							<p>{surveyData[questionNumber - 1].reponseE}</p>
+							<br />
+							<img src={surveyData[questionNumber - 1].imageE} />
+						</Link>
+					</div>
+				)}
+				{surveyData[questionNumber - 1].reponseF && (
+					<div className="grandcontainer-a">
+						<Link to={nextPath} onClick={() => saveReply(surveyData[questionNumber - 1].reponseF)}>
+							<p>{surveyData[questionNumber - 1].reponseF}</p>
+							<br />
+							<img src={surveyData[questionNumber - 1].imageF} />
+						</Link>
+					</div>
+				)}
+			</div>
+			<br />
+			<br />
+			<Link to={`/survey/${prevQuestionNumber}`}>
+				<button onClick={saveReply}>Précédent</button>
+			</Link>
+		</div>
+	);
 }
 
 export default Survey;
