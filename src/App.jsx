c0de3978dc48e3687d6bf3d66f48ff2502448275
BrebG <brebelgael@protonmail.com>
2024-05-17 10:51:43 +0200

    commit before pulling Tristan's maj

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.css";
+import "./App.scss";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>HOME</div>
-      <Link to={`survey/1`}>Go to survey</Link>
-      <Link to={`img-identification`}>Image identification</Link>
-    </>
+    <div className="home">
+      <h1>PLANTATOR</h1>
+      <div className="nav">
+        <Link to={`survey/1`} className="link">Identification par questions</Link>
+        <Link to={`img-identification`} className="link">Identification par photo</Link>
+      </div>
+    </div>
   );
 }
 
